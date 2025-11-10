c453905cff88df997b67dfa10ae7a08d3fafe68d
sadsi <sadsi@DESKTOP-A7NVA9Q>
2025-11-10 18:51:41 +0200

    primul fisier

---
+++ MagazinDispozitiveMobile/src/main/java/ro/ugal/cti/magazindispozitivemobile/MagazinDispozitiveMobile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ro.ugal.cti.magazindispozitivemobile;
+
+/**
+ *
+ * @author ai402
+ */
+public class MagazinDispozitiveMobile {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
