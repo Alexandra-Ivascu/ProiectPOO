8cd56f50e5358a58ba70dc6be113965ff844b553
Alexandra-Ivascu <>
2025-11-10 18:58:00 +0200

    clasa parinte

---
+++ MagazinDispozitiveMobile/src/main/java/ro/ugal/cti/magazindispozitivemobile/DispozitivMobil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.ugal.cti.magazindispozitivemobile;
+
+/**
+ *
+ * @author ai402
+ */
+public class DispozitivMobil {
+    private String brand;
+    private double pret;
+}
