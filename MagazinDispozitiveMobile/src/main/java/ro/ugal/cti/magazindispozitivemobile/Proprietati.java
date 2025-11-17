81326a9b050c43025ba9329352a640097f230762
Alexandra-Ivascu <alexandraioanaivascu@gmail.com>
2025-11-17 21:39:31 +0200

    adaugat interfata

---
+++ MagazinDispozitiveMobile/src/main/java/ro/ugal/cti/magazindispozitivemobile/Proprietati.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ro.ugal.cti.magazindispozitivemobile;
+
+/**
+ *
+ * @author ai402
+ */
+public interface Proprietati {
+    abstract public int numaraFunctionalitati();
+    abstract public String tipDispozitiv();
+}
